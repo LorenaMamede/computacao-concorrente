bd45c5fc6a34e20827b91cd5539506aed4ae67a7
Lorena Mamede <loremamede@gmail.com>
2021-04-04 21:37:39 -0300

    Implementando solução concorrente
    Realizando a multiplicação de matrizes com mais de uma thread.

--- Lab_2/multiplicao_matrizes.c
+++ Lab_2/multiplicao_matrizes.c
@@ -7,8 +7,11 @@
 int numero_threads;
 float *matriz;
 float *matriz_resultante;
+pthread_t *tid;
+int dimensao_matriz;
 
-void inicializar_matriz(int dimensao_matriz){
+
+void inicializar_matriz(){
 	for(int linha=0; linha<dimensao_matriz; linha++){
 		for(int coluna=0; coluna<dimensao_matriz; coluna++){
 			matriz[linha*dimensao_matriz + coluna] = 1;
@@ -18,19 +21,21 @@ void inicializar_matriz(int dimensao_matriz){
 
 }
 
-int alocar_memoria_entradas(int dimensao_matriz){
+int alocar_memoria_entradas(){
 	matriz = (float *) malloc(sizeof(float) * dimensao_matriz * dimensao_matriz);
 	if(matriz == NULL) {printf("ERRO: malloc\n"); return 0;}
 	matriz_resultante = (float *) malloc(sizeof(float) * dimensao_matriz * dimensao_matriz);
 	if(matriz_resultante == NULL) {printf("ERRO: malloc\n"); return 0;}
+	tid = (pthread_t *) malloc(sizeof(pthread_t *) * numero_threads);
+	if(tid == NULL) {printf("ERRO: malloc\n"); return 0;}
 
 	/*mat = (float *) malloc(sizeof(float) * dimensao_matriz * dimensao_matriz);
 	if(mat == NULL) {printf("ERRO: malloc\n"); return 2;}*/
 	return 1;
 }
 
-void conferir_operacoes(int dimensao_matriz){
-	puts("A matriz foi inicializada como: \n");
+void conferir_operacoes(){
+	puts("\nA matriz foi resultante foi: \n");
 	for(int linha=0; linha<dimensao_matriz; linha++){
 		for(int coluna=0; coluna<dimensao_matriz; coluna++){
 			printf("%.1f  ", matriz_resultante[linha*dimensao_matriz + coluna]);
@@ -40,8 +45,10 @@ void conferir_operacoes(int dimensao_matriz){
 
 }
 
-void multiplicar_matrizes(int dimensao_matriz){
-	for(int linha=0; linha<dimensao_matriz; linha++){
+void *  multiplicar_matrizes(void *arg){
+	int linha_thread = *(int *) arg;
+	printf("Thread %d\n", linha_thread);
+	for(int linha=linha_thread; linha<dimensao_matriz; linha+=numero_threads){
 		for(int coluna=0; coluna<dimensao_matriz; coluna++){
 			for(int parcela=0; parcela<dimensao_matriz; parcela++){
 				int indice = linha * dimensao_matriz + coluna;
@@ -52,7 +59,7 @@ void multiplicar_matrizes(int dimensao_matriz){
 }
 
 int main(int argc, char* argv[]){
-	int dimensao_matriz;
+	int *tid_local;
 
 	//verificacao e captura dos parametros de entrada
 	if(argc<3){
@@ -66,18 +73,34 @@ int main(int argc, char* argv[]){
 	if(numero_threads > dimensao_matriz) numero_threads = dimensao_matriz;
 
 	//alocacao de memoria
-	if(alocar_memoria_entradas(dimensao_matriz)){
+	if(alocar_memoria_entradas()){
 		//incializacao 
-		inicializar_matriz(dimensao_matriz);
+		inicializar_matriz();
 		
-		//multiplicacao de matrizes
-		multiplicar_matrizes(dimensao_matriz);
+		tid_local = (int *) malloc(sizeof(int) * numero_threads);
+		if(tid_local == NULL) return 2;
 
-		//confere
-		conferir_operacoes(dimensao_matriz);
+		//criacao de threads e multiplicacao de matrizes
+		for(int thread=0; thread<numero_threads; thread++){
+			printf("Criando thread %d\n", thread);	
+			*(tid_local + thread) = thread; //linha pela qual cada thread comecara
+			if(pthread_create(tid+thread, NULL, multiplicar_matrizes, (void *) (tid_local+thread))){
+				puts("ERRO ao executar pthread_create"); return 3;
+			}
+		}
+	
+		for(int thread=0; thread<numero_threads; thread++){
+			pthread_join(*(tid+thread), NULL);
+		}
 
-		return 0;
+		//printa na tela a matriz resultante
+		conferir_operacoes();
 	}else{
 		return 2;
 	}
+	free(matriz);
+	free(tid);
+	free(tid_local);
+	free(matriz_resultante);
+	return 0;
 }
