bc0d687551b2bfc6c36e9a4ea95bdff3e79a9963
Lorena Mamede <loremamede@gmail.com>
2021-05-01 18:02:25 -0300

    [Lab 5]Adicionando soma parcial e tratamento de sinal
    A forma como a entrada estava sendo recebida considerava inteiros
    negativos como dois caracteres, '-' e o número, e na conversão
    para inteiros, o número se tornava dois números. Então, agora
    o '-' é representado por uma variável sinal que muda de valor
    (1 ou -1) de acordo com o aparecimento ou não do '-'. Também
    implementando o algoritmo de soma parcial para cada posição do
    vetor.

--- Lab_5/soma_parcial_sequencial.c
+++ Lab_5/soma_parcial_sequencial.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int posicaoVetor = 0;
+int tamanhoVetor = 2;
+int sinal = 1;
+
 int* alocarVetor(int tamanhoDesejadoVetor){
 	int *vetor;
 	vetor = (int *) malloc(sizeof(int)*tamanhoDesejadoVetor);
@@ -19,11 +23,21 @@ int* realocarVetor(int *vetor, int tamanhoDesejadoVetor){
 	return vetorNovo;
 }
 
+void adicionarEntradaVetor(int *vetorEntrada, char entrada){
+	int numeroEntrada;
+	numeroEntrada = entrada - '0';
+	*(vetorEntrada+posicaoVetor) = numeroEntrada*sinal;
+	if(posicaoVetor == tamanhoVetor-1){
+		tamanhoVetor *= 2;
+		vetorEntrada = realocarVetor(vetorEntrada, tamanhoVetor);
+	}
+	if(sinal<0) sinal=1;
+	posicaoVetor++;
+}
+
 
 int main(){
 	int *vetorEntrada;
-	int posicaoVetor = 0;
-	int tamanhoVetor = 2;
 	char entrada;
 
 	vetorEntrada = alocarVetor(tamanhoVetor);
@@ -31,17 +45,19 @@ int main(){
 	puts("Forneca um vetor de entrada inserindo os numero separados por espaço\nExemplo: 1 2 3 4\n");
 	while(scanf("%c", &entrada) && (entrada != '\n')){
 		if(entrada == ' ') continue;
-		*(vetorEntrada+posicaoVetor) = entrada - '0';
-		if(posicaoVetor == tamanhoVetor-1){
-			tamanhoVetor *= 2;
-			vetorEntrada = realocarVetor(vetorEntrada, tamanhoVetor);
-		}
-		posicaoVetor++;
+		if(entrada == '-') {sinal *= -1; continue;}
+		adicionarEntradaVetor(vetorEntrada, entrada);
 	}
 
+
+	for(int i=1; i<posicaoVetor;i++){
+		*(vetorEntrada+i) += *(vetorEntrada+(i-1));
+	}
+	puts("Soma parcial de cada posicao:");
 	for(int j = 0; j < posicaoVetor; j++){
 		printf("%d, ", *(vetorEntrada+j));
 	}
+	puts("");
 
 	return 0;
 }
